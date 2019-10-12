933e3b0d1ff172c74b56620b9cfae78114b4c351
Husotsuki <husotsuki@icloud.com>
2019-10-12 15:44:22 +0200

    Modification & Final Test in branch dev with Mentor :v: :mag:

--- views/template.php
+++ views/template.php
@@ -16,7 +16,7 @@
 		 <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
 		 <!-- Custom styles for this website -->
-		 <link href="public/css/clean-blog.min.css" rel="stylesheet">
+		 <link href="public/css/clean-blog.css" rel="stylesheet">
 
 		 <!-- Tinymce -->
 	    <script src="public/js/tinymce/tinymce.min.js"></script>
