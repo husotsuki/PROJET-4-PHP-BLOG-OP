2b7a2020713ca6868ebdf3b0550cebf3c2747797
Husotsuki <husotsuki@icloud.com>
2019-10-30 15:00:49 +0100

    Modification menu

--- views/template.php
+++ views/template.php
@@ -33,7 +33,7 @@
     	 <!-- Navigation -->
 		  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
 		    <div class="container">
-		      <a class="navbar-brand" href="http://localhost/PROJET-4-PHP-BLOG-OP/index.php">Billet Simple pour l'Alaska</a>
+		      <a class="navbar-brand" href="index.php">Billet Simple pour l'Alaska</a>
 		      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 		        Menu
 		        <i class="fas fa-bars"></i>
